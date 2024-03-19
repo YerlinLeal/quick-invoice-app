52bee09aead5dd45413a3697eea2f62ac04ac371
YerlinLeal <yerlin.leal@ucr.ac.cr>
2024-03-19 01:54:45 -0600

    Se actualiza logo de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import "../assets/css/components/Footer.css"
+import logo_transparent from '../assets/img/logo_transparent.png';
 
 const Footer = (props) => {
     return <>
         {props.user &&
             <footer style={{ backgroundImage: "url(/img/footer.jpg)" }}>
                 <div className="container footer">
-                    <img className="img-logo" src='/img/Logo.png' alt='Quick Invoice' />
+                    <img className="img-logo" src={logo_transparent} alt="Quick Invoice"/>
                     <strong>Prueba técnica - Logical Data</strong>
                     <strong>Desarrollado por Yerlin Leal</strong>
                 </div>
